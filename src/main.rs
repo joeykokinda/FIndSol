d1dfc5893ce1ddb9687fbb4a1e1aff12a8556cc0
joey <jkokinda9@gmail.com>
2025-08-19 02:20:13 -0400

    checkign bal

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 use solana_sdk::signer::{keypair::Keypair, Signer};
+use solana_client::rpc_client::RpcClient;
 
 fn main() {
-    let prefix = "spek";
     let mut attempts = 0;
-    
-    println!("🔍 Searching for vanity address starting with '{}'...\n", prefix);
+    let rpc_url = "https://api.mainnet-beta.solana.com";
+    let client = RpcClient::new  (rpc_url.to_string());
+    println!("Searching for bread...\n");
     
     loop {
         let keypair = Keypair::new();
-        let publickey = keypair.pubkey().to_string();
-        
+        let pubkey = keypair.pubkey();
+        let balance = client.get_balance(&pubkey).unwrap_or(0);
+
+
         attempts += 1;
-        
-        
-        println!("Checked {} keys...", attempts);
-        
-        
-        if publickey.starts_with(prefix) {
-            println!("\n🎉 Found a match after {} attempts!", attempts);
-            println!("   => Wallet Address (Public Key): {}", publickey);
+        println!("Checked {} keys ---- ({})", attempts, pubkey );
+
+
+        if balance > 0 {
+            println!("\nYOU FOUND BAL HOLY SHIT {} ! (~{} SOL)", balance, balance as f64 / 1_000_000_000.0);
+            println!("   => Wallet Address (Public Key): {}", pubkey);
             println!("   => Private Key (Base58): {}", keypair.to_base58_string());
-            println!("\nIMPORTANT: Save this private key somewhere safe!");
+            println!("\nIt only took {}", attempts);
             break;
         }
     }
